60fce003e47109e554314b85d3fc63a7aa29209f
後藤和也 <gotoukazuya@gotoukazuyanoMacBook-Air.local>
2021-01-15 01:01:44 +0900

    kubochibi

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,15 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery")
-require("lightbox")
 
-$(window).scroll(function() {
-  var scroll = $(window).scrollTop();
-  $(".diagonal-bg svg line").attr("stroke-width",  ((30 + scroll/10)  + "%"));
-  //30 is the starting width
-  //alter the amount of growth by changing scroll/x
-});
+
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
